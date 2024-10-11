63a5ddb3fdf1daeb2d96342ecb97a74b1bdd9c59
sa@agilechai.com <sa@whitepc>
2024-10-11 13:06:06 -0400

    Update

--- driving_quiz.py
+++ driving_quiz.py
@@ -42,10 +42,9 @@ def go_to_next_question():
     st.session_state.current_question_index += 1
     st.session_state.attempts = 0
     st.session_state.show_next_button = False
-    current_question = st.session_state.current_question_index
     # Update the URL to include the new question number as a query parameter
     st.experimental_set_query_params(question=st.session_state.current_question_index + 1)
-    question_text = f"Question {current_question[0]}"
+    question_text = f"Question {st.session_state.current_question_index}"
 
 # Get query parameters and update the question number if necessary
 query_params = st.experimental_get_query_params()
